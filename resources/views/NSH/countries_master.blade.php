26346e722a5c244298d916fca7fd52c496d07a1f
drumilpatel1586 <drumilpatel1586@gmail.com>
2024-05-16 13:35:29 +0530

    mod-16-05-24

--- resources/views/NSH/countries_master.blade.php
+++ resources/views/NSH/countries_master.blade.php
@@ -6,3 +6,25 @@
 
 @endsection
 
+@push('js')
+<script>
+    $(document).ready(function() {
+        $('#searchButton').click(function() {
+            var searchText = $('#searchInput').val().toLowerCase();
+
+            $('tbody tr').each(function() {
+                var countryName = $(this).find('td:eq(1)').text().toLowerCase();
+                var shortName = $(this).find('td:eq(2)').text().toLowerCase();
+                var phoneCode = $(this).find('td:eq(3)').text().toLowerCase();
+
+                if (countryName.includes(searchText) || shortName.includes(searchText) ||
+                    phoneCode.includes(searchText)) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        });
+    });
+</script>
+@endpush
--- resources/views/livewire/countries-master.blade.php
+++ resources/views/livewire/countries-master.blade.php
@@ -23,24 +23,31 @@
                     <div class="col-12">
                         <div class="card">
 
-                            {{-- <div class="card-header">
-                                <h3 class="card-title">DataTable with minimal features &amp; hover style</h3>
-                            </div> --}}
-
                             <div class="card-body">
                                 <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
                                     <div class="row">
                                         <div class="col-sm-12 col-md-6"></div>
                                         <div class="col-sm-12 col-md-6"></div>
                                     </div>
-                                    
+
                                     <div class="row">
                                         <div class="col-sm-12">
-                                            <table class="table table-striped table-hover text-center ">
+
+                                            <div class="row mb-3">
+                                                <div class="col-md-11">
+                                                    <input type="text" class="form-control" id="searchInput"
+                                                        placeholder="Search...">
+                                                </div>
+                                                <div class="col-md-1">
+                                                    <button class="btn btn-primary" id="searchButton">Search</button>
+                                                </div>
+                                            </div>
+
+                                            <table class="table table-striped table-hover text-center">
                                                 <thead>
                                                     <tr>
                                                         <th scope="col">No.</th>
-                                                        <th scope="col">country</th>
+                                                        <th scope="col">Country</th>
                                                         <th scope="col">Short Name</th>
                                                         <th scope="col">Phone Code</th>
                                                         <th scope="col">Active Status</th>
@@ -54,20 +61,21 @@
                                                             <td>{{ $country->shortname }}</td>
                                                             <td>{{ $country->phonecode }}</td>
                                                             <td style="cursor: pointer;">
-                                                                <a href="country/{{$country->id}}">
-                                                                @if ($country->is_active)
-                                                                <!-- Green tick -->
-                                                                    <i class="fa fa-check-circle text-success"></i>
-                                                                @else
-                                                                <!-- Red cross -->
-                                                                    <i class="fa fa-times-circle text-danger"></i>
-                                                                @endif
-                                                            </a>
+                                                                <a href="country/{{ $country->id }}">
+                                                                    @if ($country->is_active)
+                                                                        <!-- Green tick -->
+                                                                        <i class="fa fa-check-circle text-success"></i>
+                                                                    @else
+                                                                        <!-- Red cross -->
+                                                                        <i class="fa fa-times-circle text-danger"></i>
+                                                                    @endif
+                                                                </a>
                                                             </td>
                                                         </tr>
                                                     @endforeach
                                                 </tbody>
                                             </table>
+
                                         </div>
                                     </div>
                                 </div>
