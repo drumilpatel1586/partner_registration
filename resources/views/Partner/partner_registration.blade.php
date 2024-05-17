1ef845eec51ed95bda126362cfc2f2fc7e8cdc00
drumilpatel1586 <drumilpatel1586@gmail.com>
2024-05-17 10:28:52 +0530

    mod-17-05-24

--- resources/views/Partner/partner_registration.blade.php
+++ resources/views/Partner/partner_registration.blade.php
@@ -9,15 +9,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </link>
 
     {{-- css file --}}
     <link rel="stylesheet" type="text/css" href="{{ asset('css/partner_registration.css') }}">
 
-    {{-- font awsome link --}}
+    {{-- font awesome link --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
 
-    {{-- Jquery CDN --}}
+    {{-- jQuery CDN --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <link rel="icon" href="https://crm.qntmnet.com/QN-CDN/images/favicon.png" type="image/x-icon">
@@ -44,7 +43,7 @@
 
             <form class="row g-3 p-4" method="post" action="{{ route('partner.registration') }}">
                 @csrf
-               
+
                 {{-- General Information --}}
                 <div class="col-md-12">
                     <span class="form-span">General Information</span>
@@ -52,11 +51,13 @@
                 </div>
 
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="company_name" value="{{ old('company_name') }}" placeholder="Company Name">
+                    <input type="text" class="form-control" name="company_name" value="{{ old('company_name') }}"
+                        placeholder="Company Name">
                     <span class="text-danger">{{ $errors->first('company_name') }}</span>
                 </div>
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="address" value="{{old('address')}}" placeholder="Address">
+                    <input type="text" class="form-control" name="address" value="{{ old('address') }}"
+                        placeholder="Address">
                     <span class="text-danger">{{ $errors->first('address') }}</span>
                 </div>
 
@@ -66,7 +67,7 @@
                         aria-label="Default select example">
                         <option selected disabled>Select Country</option>
                         @foreach ($countries as $country)
-                            <option value="{{ $country->id }}" data-country-shortname="{{ $country->shortname }}">
+                            <option value="{{ $country->id }}" data-country-phonecode="{{ $country->phonecode }}">
                                 {{ $country->name }}
                             </option>
                         @endforeach
@@ -92,16 +93,19 @@
                     <span class="text-danger">{{ $errors->first('select_city') }}</span>
                 </div>
                 <div class="col-md-6">
-                    <input type="tel" maxlength="6" class="form-control" name="zip" value="{{old('zip')}}" placeholder="Zip">
+                    <input type="tel" maxlength="6" class="form-control" name="zip"
+                        value="{{ old('zip') }}" placeholder="Zip">
                     <span class="text-danger">{{ $errors->first('zip') }}</span>
                 </div>
 
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="website" value="{{old('website')}}" placeholder="Website">
+                    <input type="text" class="form-control" name="website" value="{{ old('website') }}"
+                        placeholder="Website">
                     <span class="text-danger">{{ $errors->first('website') }}</span>
                 </div>
                 <div class="col-md-6">
-                    <input type="tel" maxlength="15" class="form-control" value="{{old('landline')}}" name="landline" placeholder="Landline">
+                    <input type="tel" maxlength="15" class="form-control" value="{{ old('landline') }}"
+                        name="landline" placeholder="Landline">
                     <span class="text-danger">{{ $errors->first('landline') }}</span>
                 </div>
                 {{-- General Information End --}}
@@ -113,54 +117,51 @@
                 </div>
 
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="first_name" value="{{old('first_name')}}" placeholder="First Name">
+                    <input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}"
+                        placeholder="First Name">
                     <span class="text-danger">{{ $errors->first('first_name') }}</span>
                 </div>
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="last_name" value="{{old('last_name')}}" placeholder="Last Name">
+                    <input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}"
+                        placeholder="Last Name">
                     <span class="text-danger">{{ $errors->first('last_name') }}</span>
                 </div>
 
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="job_title" value="{{old('job_title')}}" placeholder="Job Title">
+                    <input type="text" class="form-control" name="job_title" value="{{ old('job_title') }}"
+                        placeholder="Job Title">
                     <span class="text-danger">{{ $errors->first('job_title') }}</span>
                 </div>
                 <div class="col-md-6">
                     <div class="mobile_no form-control">
-
-                        <img class="flag" id="flagContainer" src="">
-
+                        <span class="text-muted" id="countrycode"></span>
                         <input style="border-color: transparent; outline: none;" maxlength="10" name="mobile"
-                            type="tel" class="mobile" value="{{old('mobile')}}" placeholder="Mobile">
-
-
+                            type="tel" class="mobile" value="{{ old('mobile') }}" placeholder="Mobile">
                     </div>
                     <span class="text-danger">{{ $errors->first('mobile') }}</span>
                 </div>
 
                 <div class="col-md-6">
-                    <input type="text" maxlength="15" value="{{old('personal_landline')}}" class="form-control" name="personal_landline"
-                        placeholder="Landline">
+                    <input type="text" maxlength="15" value="{{ old('personal_landline') }}"
+                        class="form-control" name="personal_landline" placeholder="Landline">
                     <span class="text-danger">{{ $errors->first('personal_landline') }}</span>
                 </div>
                 <div class="col-md-6">
-                    <input type="mail" class="form-control" value="{{old('mail')}}" name="mail" placeholder="Email">
+                    <input type="mail" class="form-control" value="{{ old('mail') }}" name="mail"
+                        placeholder="Email">
                     <span class="text-danger">{{ $errors->first('mail') }}</span>
                 </div>
 
                 {{-- Contact Information End --}}
 
                 <div class="captcha_img">
-
                     <div class="col-12">
                         <span>{!! captcha_img('math') !!}</span>
                         <button type="button" class="reload" id="reload">&#x21bb;</button>
                     </div>
-
                 </div>
 
                 <div class="partner_form_footer">
-
                     <div class="col-6">
                         <input type="text" class="form-control" name="captcha" placeholder="Enter Captcha">
                         <span class="text-danger">{{ $errors->first('captcha') }}</span>
@@ -169,106 +170,13 @@
                     <div class="col-6 submit_btn">
                         <button type="submit" class="btn">Submit</button>
                     </div>
-
                 </div>
-
             </form>
         </section>
     </div>
 
     {{-- #################################  js files  ################################## --}}
-
-    {{-- js for captcha --}}
-    <script>
-        $('#reload').click(function() {
-            $.ajax({
-                type: 'GET',
-                url: 'reloadCaptcha',
-                success: function(data) {
-                    $('.captcha_img span').html(data.captcha);
-                }
-            })
-        })
-    </script>
-
-    {{-- js for selected country flag and mobileno. --}}
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const countrySelect = document.getElementById('select_country');
-            const flagImg = document.getElementById('flagContainer');
-
-            countrySelect.addEventListener('change', function() {
-                const selectedOption = countrySelect.options[countrySelect.selectedIndex];
-                const countryShortname = selectedOption.getAttribute('data-country-shortname');
-
-                if (countryShortname) {
-                    // Set the src attribute of the flagImg
-                    flagImg.src = `https://flagsapi.com/${countryShortname}/shiny/32.png`;
-                } else {
-                    // Clear the src attribute if no country is selected
-                    flagImg.src = '';
-                }
-            });
-        });
-    </script>
-
-    {{-- js for country flag and mobileno. --}}
-    <script>
-        const input = document.querySelector("#phone");
-        window.intlTelInput(input, {
-            utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@22.0.2/build/js/utils.js",
-        });
-    </script>
-
-    {{-- js for county state and city filter --}}
-    <script>
-        // Include this JavaScript in your view or in a separate file
-        $(document).ready(function() {
-
-            $('select[name="select_country"]').change(function() {
-                var country_id = $(this).val();
-                if (country_id) {
-                    $.ajax({
-                        url: '/states/' + country_id,
-                        type: "GET",
-                        dataType: "json",
-                        success: function(data) {
-                            $('select[name="select_state"]').empty();
-                            $.each(data, function(key, value) {
-                                $('select[name="select_state"]').append(
-                                    '<option value="' + value.id + '">' + value
-                                    .name + '</option>');
-                            });
-                        }
-                    });
-                } else {
-                    $('select[name="select_state"]').empty();
-                }
-            });
-
-            $('select[name="select_state"]').change(function() {
-                var state_id = $(this).val();
-                if (state_id) {
-                    $.ajax({
-                        url: '/cities/' + state_id,
-                        type: "GET",
-                        dataType: "json",
-                        success: function(data) {
-                            $('select[name="select_city"]').empty();
-                            $.each(data, function(key, value) {
-                                $('select[name="select_city"]').append(
-                                    '<option value="' + value.id + '">' + value
-                                    .name + '</option>');
-                            });
-                        }
-                    });
-                } else {
-                    $('select[name="select_city"]').empty();
-                }
-            });
-        });
-    </script>
-
+    <script src="{{ asset('js/partner_registration.js') }}"></script>
 </body>
 
 </html>
